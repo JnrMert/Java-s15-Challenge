c2b3236588ba13518f8ebb5b781feaf05468a507
Mert Yazıcı <yzc.mert@icloud.com>
2024-10-16 17:05:41 +0300

    Know issues: Main

---
+++ src/com/workintech/challenge/util/EnumTypes.java
@@ -0,0 +1,15 @@
+package com.workintech.challenge.util;
+
+public class EnumTypes {
+    public enum BookStatus {
+        AVAILABLE, BORROWED, RESERVED, MAINTENANCE
+    }
+
+    public enum UserRole {
+        READER, LIBRARIAN, ADMIN, AUTHOR
+    }
+    public enum Status {
+        IN_STOCK,
+        NOT_IN_STOCK,
+    }
+}
